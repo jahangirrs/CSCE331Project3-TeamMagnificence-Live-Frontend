d8483880a367442589e0c8704423c0da14f75563
Janik Samandov <jahangirrs@tamu.edu>
2025-03-29 23:40:56 -0500

    Added React changes from previous commit: completed basic manager views

--- FrontEnd/reactfirst/src/App.tsx
+++ FrontEnd/reactfirst/src/App.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import React from 'react'
 
-function GetEmployeeTable(){
+//Fetch and Build employee Table
+function EmployeeView(){
   const [employees, setEmployees] = useState("");
 
   //API call to backend for employee data
   React.useEffect(() => {
-    fetch("/manager")
+    fetch("/manager/employees")
       .then((res) => res.json())
       .then((data) => setEmployees(data)
       
@@ -62,12 +63,291 @@ function GetEmployeeTable(){
   )
 }
 
+//Fetch and build inventory table
+function InventoryView(){
+  const [inventory, setInventory] = useState("");
+
+  //API call to backend for inventory data
+  React.useEffect(() => {
+    fetch("/manager/inventory")
+    .then((res) => res.json())
+    .then((data) => setInventory(data))
+    .catch(e => console.log(e))
+  }, []);
+
+  var inventoryData = JSON.parse(JSON.stringify(inventory));
+  let rows = new Array();
+  for(var i in inventoryData){
+    rows.push([inventoryData[i].id, inventoryData[i].name, inventoryData[i].unitsize, 
+      inventoryData[i].stocknum.toFixed(2), inventoryData[i].stockpercent.toFixed(2), 
+      inventoryData[i].idealstock, inventoryData[i].itemprice, 
+      inventoryData[i].supplier, inventoryData[i].islow.toString()]);
+  }
+
+  return(
+    <>
+      <h1>Inventory</h1>
+    
+      <table>
+
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Unit Size</th>
+            <th>Stock Num</th>
+            <th>Stock Percent</th>
+            <th>Ideal Stock</th>
+            <th>Item Price</th>
+            <th>Supplier</th>
+            <th>Low</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map( (row:any) =>
+            <tr key = {row}>{
+              row.map( (data:any) =>
+              
+                <td key = {data + Math.random()}>{data}</td>
+              
+              )
+
+            }</tr>
+          
+          )}
+        </tbody>
+
+      </table>
+    
+    
+    
+    </>
+
+
+  );
+}
+
+//Fetch and build Menu table
+function MenuView(){
+  const [menu, setMenu] = useState("");
+
+  //API call to backend for menu data
+  React.useEffect(() => {
+    fetch("/manager/menu")
+    .then((res) => res.json())
+    .then((data) => setMenu(data))
+    .catch(e => console.log(e))
+  }, []);
+
+  var menuData = JSON.parse(JSON.stringify(menu));
+  let rows = new Array();
+  for(var i in menuData){
+    rows.push([menuData[i].id, menuData[i].item_name, menuData[i].base_cost, 
+      menuData[i].item_group]);
+  }
+
+  return(
+    <>
+      <h1>Menu</h1>
+    
+      <table>
+
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Item</th>
+            <th>Cost</th>
+            <th>Group</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map( (row:any) =>
+            <tr key = {row}>{
+              row.map( (data:any) =>
+                
+                <td key = {data + Math.random()}>{data}</td>
+              )
+            }</tr>
+
+          )}
+        </tbody>
+      </table>
+    
+    </>
+  );
+}
+
+//Fetch and build hourly sales table
+function HourlySalesView(){
+  const [hourlySales, setHourlySales] = useState("");
+
+  //API call to backend for sales data
+  React.useEffect(() => {
+    fetch("/manager/hourlySales")
+    .then((res) => res.json())
+    .then((data) => setHourlySales(data))
+    .catch(e => console.log(e))
+  }, []);
+
+  var hourlySalesData = JSON.parse(JSON.stringify(hourlySales));
+  let rows = new Array();
+  for(var i in hourlySalesData){
+    rows.push([hourlySalesData[i].sales, hourlySalesData[i].hour]);
+  }
+
+  return(
+
+    <>
+      <h1>Hourly Sales</h1>
+
+      <table>
+
+        <thead>
+          <tr>
+            <th>Sales</th>
+            <th>Hour</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map( (row:any) =>
+            <tr key = {row}>{
+              row.map( (data:any) =>
+              
+                  <td key = {data + Math.random()}>{data}</td>
+              )
+              }</tr>
+          )}
+        </tbody>
+      </table>
+    
+    </>
+
+  );
+}
+
+function PurchaseOrderView(){
+  const [purchaseOrders, setPurchaseOrders] = useState("");
+
+  //API call to backend for purchase order data
+  React.useEffect(() => {
+    fetch("/manager/purchaseOrder")
+    .then((res) => res.json())
+    .then((data) => setPurchaseOrders(data))
+    .catch(e => console.log(e))
+  }, []);
+
+  var purchaseOrdersData = JSON.parse(JSON.stringify(purchaseOrders));
+  let rows = new Array();
+  for(var i in purchaseOrdersData){
+    rows.push([purchaseOrdersData[i].inventory_name,
+      purchaseOrdersData[i].unitsize, purchaseOrdersData[i].supplier_name,
+      purchaseOrdersData[i].order_date, purchaseOrdersData[i].quantity,
+      purchaseOrdersData[i].cost_per_unit, purchaseOrdersData[i].total_cost,
+      purchaseOrdersData[i].order_status, purchaseOrdersData[i].expected_arrival,
+      purchaseOrdersData[i].received_date])
+  }
+
+
+  return(
+    <>
+
+      <h1>Purchase Orders</h1>
+
+        <table>
+
+          <thead>
+            <tr>
+              <th>Inventory Item</th>
+              <th>Unit Size</th>
+              <th>Supplier</th>
+              <th>Order Date</th>
+              <th>Amount</th>
+              <th>Price per Unit</th>
+              <th>Total Price</th>
+              <th>Status</th>
+              <th>Expected Arrival</th>
+              <th>Received</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map( (row:any) =>
+            
+              <tr key = {row}>{
+                row.map( (data:any) =>
+                
+                  <td key = {data + Math.random()}>{data}</td>
+                )
+                
+              }</tr>
+            
+            )}
+          </tbody>
+        </table>
+
+    </>
+
+  );
+}
+
 function App() {
+  //Tab display values
+  const [displayEmployee, setDisplayEmployee] = useState(false);
+  const [displayInventory, setDisplayInventory] = useState(false);
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const [displayHourlySales, setDisplayHourlySales] = useState(false);
+  const [displayPurchaseOrders, setDisplayPurchaseOrders] = useState(false);
+
+  //Hide all views
+  function hideAll(){
+    setDisplayEmployee((displayEmployee) => false);
+    setDisplayInventory((displayInventory) => false);
+    setDisplayMenu((displayMenu) => false);
+    setDisplayHourlySales((displayHourlySales) => false);
+    setDisplayPurchaseOrders((displayPurchaseOrders) => false);
+  }
+
+  //Toggle view on button clicks
+  function toggleEmployees(){
+    hideAll();
+    setDisplayEmployee((displayEmployee) => !displayEmployee);
+  }
+
+  function toggleInventory(){
+    hideAll();
+    setDisplayInventory((displayInventory) => !displayInventory);
+  }
+
+  function toggleMenu(){
+    hideAll();
+    setDisplayMenu((displayMenu) => !displayMenu);
+  }
+
+  function toggleHourlySales(){
+    hideAll();
+    setDisplayHourlySales((displayHourlySales) => !displayHourlySales);
+  }
+
+  function togglePurchaseOrders(){
+    hideAll();
+    setDisplayPurchaseOrders((displayPurchaseOrders) => !displayPurchaseOrders);
+  }
 
   return (
     <>
-
-      <GetEmployeeTable />
+      <div>
+      <button onClick = {toggleEmployees}>Employees</button>
+      <button onClick = {toggleInventory}>Inventory</button>
+      <button onClick = {toggleMenu}>Menu</button>
+      <button>Sales Data</button>
+      <button onClick = {toggleHourlySales}>Hourly Sales</button>
+      <button onClick = {togglePurchaseOrders}>Purchase Order</button>
+      </div>
+      {displayEmployee && <EmployeeView />}
+      {displayInventory && <InventoryView />}
+      {displayMenu && <MenuView />}
+      {displayHourlySales && <HourlySalesView />}
+      {displayPurchaseOrders && <PurchaseOrderView/>}
     </>
   )
 }
